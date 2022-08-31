cd7470b3e5995a6385ea5eecfb0538cc217f7476
Michael Schuchowski <mschuchowski@gmx.de>
2022-08-31 15:58:25 +0200

    registration with logged in account

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   root to: "pages#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
