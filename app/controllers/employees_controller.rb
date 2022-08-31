b1efad73f35893f526ca125855fe6564af4708d7
Christian von Storch <cvs.xowii@gmail.com>
2022-08-31 10:25:50 +0200

    employees controller without user authentication

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,29 @@
+class EmployeesController < ApplicationController
+  # before_action :authorize_user
+  # in theory any of the actions associated with this controller will only be
+  # available to the manager, it will only go through the managers journey so
+  # authentication shant be necessary
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @user = User.all
+  end
+
+  def delete
+    User.find(params[:id]).destroy
+    redirect_to_users_path, status: :see_other
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def user_params
+
+    params.require(:user)
+  end
+
+end
