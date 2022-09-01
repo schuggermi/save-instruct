67dc868ed4aa500482e026a122c3f5f5b85f78f1
buluttuncel <tuncelbu@gmail.com>
2022-09-01 17:35:13 +0100

    testing

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,4 +1,9 @@
 class Step < ApplicationRecord
   belongs_to :task
   validates :instruction, presence: true, length: { minimum: 10 }
+  after_create :assign_order
+
+  def assign_order
+    update(order: task.steps.last.order.nil? ? 1 : task.steps.last.order + 1)
+  end
 end
