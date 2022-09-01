d1b84e7e02886a7906198cf80dc2da98051223bd
Michael Schuchowski <mschuchowski@gmx.de>
2022-09-01 10:23:37 +0200

    fixing something

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,5 @@ class Task < ApplicationRecord
   has_many :employee_tasks, dependent: :destroy
   has_many :users, through: :employee_tasks
   accepts_nested_attributes_for :steps, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :employee_tasks
 end
