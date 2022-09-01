e39ca9d8d7655075782616f15580d6d9c72a25f8
buluttuncel <tuncelbu@gmail.com>
2022-09-01 18:02:08 +0100

    absolutely lovely

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,9 +1,10 @@
 class Step < ApplicationRecord
   belongs_to :task
   validates :instruction, presence: true, length: { minimum: 10 }
-  after_create :assign_order
+  after_commit :assign_order, on: %i[create]
 
   def assign_order
-    update(order: task.steps.last.order.nil? ? 1 : task.steps.last.order + 1)
+    self.order = task.steps.last.order.nil? ? 1 : task.steps.last.order + 1
+    send(update_without_callbacks)
   end
 end
