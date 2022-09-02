422856487ad07fab9aa60038d38b0b53bb0e1536
buluttuncel <tuncelbu@gmail.com>
2022-09-02 12:54:39 +0100

    removed unnecessary comments and added stim controller

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,13 +1,4 @@
 class Step < ApplicationRecord
   belongs_to :task
   validates :instruction, presence: true, length: { minimum: 10 }
-  # after_create :assign_order_to_steps
 end
-
-# Step.by_date
-
-# def assign_order_to_steps
-#   order_numbers = [1..100].first(steps.count)
-#   task.steps.each do |step|
-#     step.order
-#   end
