c544b0811066e60c58632e7c0791c7c27bdf3fbb
Christian von Storch <cvs.xowii@gmail.com>
2022-09-02 15:32:30 +0200

    user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :employee_task, dependent: :destroy
+  has_many :employee_tasks, dependent: :destroy
 
   has_one_attached :photo
 
