4419c3e6c84a0b62106358e2e2ac88da2e1bfd5a
buluttuncel <tuncelbu@gmail.com>
2022-09-02 18:15:29 +0100

    managed to connect controller

--- app/javascript/controllers/order_by_drag_controller.js
+++ app/javascript/controllers/order_by_drag_controller.js
@@ -1,30 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
-import { Sortable } from "index"
+import { Sortable } from "stimulus-sortable"
 
 
 // Connects to data-controller="order-by-drag"
 export default class extends Controller {
   connect() {
     console.log("Hello!")
-    // this.sortable = Sortable.create(this.element, {
-    //   onEnd: this.end.bind(this),
-    // });
+    };
   }
 
-  end(event) {
-    console.log("Hello!")
-    event.preventDefault()
-    // let id = event.item.dataset.id;
-    // let data = new FormData();
-    // data.append("position", event.newIndex + 1);
-    // Rails.ajax({
-    //   url: this.data.get("url").replace(":id", id),
-    //   type: "PATCH",
-    //   data: data,
-    // });
-  }
-}
-
+//   end(event) {
+//     console.log("Hello!")
+//     event.preventDefault()
+//     // let id = event.item.dataset.id;
+//     // let data = new FormData();
+//     // data.append("position", event.newIndex + 1);
+//     // Rails.ajax({
+//     //   url: this.data.get("url").replace(":id", id),
+//     //   type: "PATCH",
+//     //   data: data,
+//     // });
+//   }
+// }
 
 
 
