724b5ba413aa8524803ffbe8328e19093c1462d7
buluttuncel <tuncelbu@gmail.com>
2022-09-02 16:28:25 +0100

    working on connecting controller

--- app/javascript/controllers/order_by_drag_controller.js
+++ app/javascript/controllers/order_by_drag_controller.js
@@ -1,7 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
+import { Sortable } from "index"
+
 
 // Connects to data-controller="order-by-drag"
 export default class extends Controller {
   connect() {
+    console.log("Hello!")
+    // this.sortable = Sortable.create(this.element, {
+    //   onEnd: this.end.bind(this),
+    // });
+  }
+
+  end(event) {
+    console.log("Hello!")
+    event.preventDefault()
+    // let id = event.item.dataset.id;
+    // let data = new FormData();
+    // data.append("position", event.newIndex + 1);
+    // Rails.ajax({
+    //   url: this.data.get("url").replace(":id", id),
+    //   type: "PATCH",
+    //   data: data,
+    // });
   }
 }
+
+
+
+
+
+// on end
