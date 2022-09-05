f12d961fbea5f712c8f5c4d6cd69c9754339045c
Michael Schuchowski <mschuchowski@gmx.de>
2022-09-05 18:00:56 +0200

    changed seeding only missing images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'faker'
+require 'open-uri'
+require 'mechanize'
+require 'nokogiri'
+
+puts `clear`
+puts "Cleaning database..."
+User.destroy_all
+Task.destroy_all
+
 admin = User.create(
   first_name: "Max",
   last_name: "Mustermann",
@@ -22,17 +32,7 @@ employee = User.create(
   password: "123456"
 )
 
-puts "=============================="
-puts "One Manager account has been generated!"
-puts admin.email
-puts admin.password
-puts "=============================="
-
-puts "=============================="
-puts "One Employee account has been generated!"
-puts employee.email
-puts employee.password
-puts "=============================="
+puts `clear`
 
 task = Task.create(
   name: "Drain the water from the pretreatment tubs",
@@ -67,6 +67,20 @@ step_four = Step.create(
   order: 4
 )
 
+puts `clear`
+
+puts "=============================="
+puts "One Manager account has been generated!"
+puts admin.email
+puts admin.password
+puts "=============================="
+
+puts "=============================="
+puts "One Employee account has been generated!"
+puts employee.email
+puts employee.password
+puts "=============================="
+
 puts "=============================="
 puts "Inserted Task for testing with 4 Steps"
 puts "#{task.name} | #{task.description}"
@@ -76,3 +90,38 @@ puts "#{step_two.instruction} | #{step_two.info}"
 puts "#{step_three.instruction} | #{step_three.info}"
 puts "#{step_four.instruction} | #{step_four.info}"
 puts "=============================="
+
+puts "Start inserting 20 random Employees with Tasks assigned..."
+sleep(3)
+
+agent = Mechanize.new
+agent.get("https://www.linkedin.com/login/de?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin") do |page|
+  linkedin = page.form_with(action: '/checkpoint/lg/login-submit') do |form|
+    form['user[session_key]'] = "mschuchowski@gmx.de",
+    form['user[session_password]'] = "0111MiSchu1999"
+  end.submit
+end
+
+diehl_defence = agent.get("https://www.linkedin.com/company/diehl-defence/people/")
+doc = Nokogiri::HTML(diehl_defence.content.toutf8)
+h1 = doc.xpath('h1').text
+puts h1
+
+# 5.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   file = URI.open(Faker::LoremFlickr.image(size: "300x300", search_terms: ['profile'], match_all: true))
+
+#   employee = User.create(
+#     first_name: first_name,
+#     last_name: last_name,
+#     rank: Faker::Job.title, admin: false,
+#     email: Faker::Internet.email(name: "#{first_name} #{last_name}", separators: ".", domain: "mail"),
+#     password: "123456"
+#   )
+
+#   employee.photo.attach(io: file, filename: "#{employee.first_name}_#{employee.last_name}.png", content_type: "image/png")
+#   employee.save
+
+#   puts "Inserted Employee #{employee.email}"
+# end
