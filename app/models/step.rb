7600da17602b631408c73f885622a183b9dd95cb
Michael Schuchowski <mschuchowski@gmx.de>
2022-09-07 16:03:11 +0200

    fixes buttons back and search also employee profile

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,5 +1,5 @@
 class Step < ApplicationRecord
   belongs_to :task
-  validates :instruction, presence: true, length: { minimum: 10 }
+  validates :instruction, presence: true
   acts_as_list column: :order, scope: :task
 end
